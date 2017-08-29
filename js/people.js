2f0977ca50ad2116eced83c7caadc48c4d87c324
Jasmine Lu (jll68) <jll68@duke.edu>
2017-08-29 23:17:47 +0200

    added overlays on people page, popup not updated yet - js for peoples page still in development

---
+++ js/people.js
@@ -0,0 +1,9 @@
+(function(){
+  var overlays = document.getElementsByClassName('nameoverlay');
+  for(var i = 0; i < overlays.length; i++){
+    if(overlays[i].getElementsByClassName("nameText")[0].getElementById('name').innerText == 'Jasmine Lu'){
+      
+
+    }
+  }
+}) ();
