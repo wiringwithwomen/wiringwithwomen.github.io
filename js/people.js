6f4aef405fff906fd81c5c2944da5ad186fcb362
Jasmine Lu (jll68) <jll68@duke.edu>
2017-08-30 15:13:04 +0200

    fix to overlays, responsive width adjustment

--- js/people.js
+++ js/people.js
@@ -1,13 +1,13 @@
 (function(){
   var images = document.getElementsByClassName('execimage');
 
-  var names = ['Jasmine Lu', 'Sarah Zhou', 'Emily Liu', 'Anika Mukherji',
-   'Aninda Manocha', 'Alethea Toh', 'Cyan DeVeaux'];
-  var positions = ['President', 'Vice President', 'Outreach External',
-  'Outreach Internal', 'Treasurer', 'Lean In Coordinator', 'Marketing Chair']
-  var bios = ['Hi everyone! I am currently a junior double majoring in ECE/CS and I\'m really passionate about doing projects related to the intersection between technology and other fields (health, humanities, art, etc). I am so excited to work with this team of lovely ladies to foster a stronger community among the women in tech at Duke. If you ever have any ideas or suggestions about things we can do as a group, feel free to reach out! I also love grabbing lunch to get to know people&nbsp;:)<br><br>CS Courses: Data Structures and Algorithms, Software Design for Biomedical Devices, Computer Architecture<br>Email: j.lu@duke.edu',
-  'Hi everyone! I am currently a junior double majoring in ECE/CS and I\'m really passionate about doing projects related to the intersection between technology and other fields (health, humanities, art, etc). I am so excited to work with this team of lovely ladies to foster a stronger community among the women in tech at Duke. If you ever have any ideas or suggestions about things we can do as a group, feel free to reach out! I also love grabbing lunch to get to know people&nbsp;:)<br><br>CS Courses: Data Structures and Algorithms, Computer Architecture, Software Engineering Design<br>Email: sarah.zhou@duke.edu'
-  ];
+  var overlays = document.getElementsByClassName('nameoverlay');
+  for(var i = 0; i < overlays.length; i++){
+    overlays[i].setAttribute("style","width:"+images[0].width + "px");
+    console.log(images[0].width);
+    console.log(images[0].height);
+    console.log(overlays[i].width);
+  }
   images[0].onclick = function () {jasmine();};
   images[1].onclick = function() {sarah();};
   images[2].onclick = function(){emily();};
